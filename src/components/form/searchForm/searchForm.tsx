f80967f0e188c80fb5da5de088f501f8985b0bfa
Mauzek <artemilliy@gmail.com>
2025-07-12 02:20:58 +0300

    refactor(searchForm): изменить струкруту searchForm используя компоненты для пк и моб

--- src/components/form/searchForm/searchForm.tsx
+++ src/components/form/searchForm/searchForm.tsx
@@ -1,6 +1,16 @@
+import { DesktopSearchForm } from "./desktop";
+import { MobileSearchForm } from "./mobile";
+import styles from "./SearchForm.module.scss";
 
-export const searchForm = () => {
+export const SearchForm = () => {
   return (
-    <div>searchForm</div>
-  )
-}
+    <div className={styles.searchFormContainer}>
+      <div className={styles.searchFormContainer__desktop}>
+        <DesktopSearchForm />
+      </div>
+      <div className={styles.searchFormContainer__mobile}>
+        <MobileSearchForm />
+      </div>
+    </div>
+  );
+};
