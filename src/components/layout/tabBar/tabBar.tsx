b9cdbf47b64f4e24e0a2e8cd1e8828c1d44f3169
Mauzek <artemilliy@gmail.com>
2025-07-12 13:06:01 +0300

    feat(tabBar): добавить компонент tabBar для мобильных устройств

---
+++ src/components/layout/tabBar/tabBar.tsx
@@ -0,0 +1,56 @@
+import { Link, useLocation } from "react-router-dom";
+import {
+  Icon28HomeOutline,
+  Icon28FilmStripOutline,
+  Icon28LikeOutline,
+} from "@vkontakte/icons";
+import styles from "./tabBar.module.scss";
+
+const tabs = [
+  {
+    path: "/",
+    icon: <Icon28HomeOutline />,
+    text: "Главная",
+  },
+  {
+    path: "/movies",
+    icon: <Icon28FilmStripOutline />,
+    text: "Фильмы",
+  },
+  {
+    path: "/favourite",
+    icon: <Icon28LikeOutline />,
+    text: "Избранное",
+  },
+];
+
+export const TabBar = () => {
+  const location = useLocation();
+
+  const isActiveLink = (path: string) => {
+    return location.pathname === path;
+  };
+
+  return (
+    <div className={styles.tabBar}>
+      <nav className={styles.tabBar__nav}>
+        {tabs.map((tab) => (
+          <Link
+            key={tab.path}
+            to={tab.path}
+            className={`${styles.tabBar__item} ${
+              isActiveLink(tab.path) ? styles["tabBar__item--active"] : ""
+            }`}
+          >
+            <div className={styles.tabBar__icon}>
+              {tab.icon}
+            </div>
+            <span className={styles.tabBar__text}>
+              {tab.text}
+            </span>
+          </Link>
+        ))}
+      </nav>
+    </div>
+  )
+}
