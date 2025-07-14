102b55e8288598977bcb924c69296a4801776559
Mauzek <artemilliy@gmail.com>
2025-07-14 12:57:56 +0300

    refactor(components): обновить index`ы для экспорта

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,5 @@
 export * from './filterBar';
 export * from './section';
 export * from './moviesGrid';
+export * from './movieDetails';
+export * from './favoriteModal';
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,6 @@
 export * from "./filter";
 export * from "./grid";
-export * from './movieCard'
+export * from './movieCard';
+export * from './modal';
+export * from './skeleton';
+export * from './emptyState';
