8cc21212d08364525af3f1481073948ee9ebcfba
Mauzek <artemilliy@gmail.com>
2025-07-14 14:52:46 +0300

    feat(notFoundPage): добавить страницу notFoundPage

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { ErrorState } from "@/components";
+import { useEffect } from "react";
 
 export const NotFoundPage = () => {
+  useEffect(() => {
+    document.title = "Страница не найдена 404 - VK FilmsLib";
+  }, []);
   return (
-    <section>
-        404
-    </section>
-  )
-}
+    <main role="main" aria-label="Страница не найдена 404 - VK FilmsLib">
+      <ErrorState error="404" />
+    </main>
+  );
+};
