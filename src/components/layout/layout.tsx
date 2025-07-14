ce469cc9a902e30ddab7972774a6ad17f2dc58d6
Mauzek <artemilliy@gmail.com>
2025-07-14 12:56:34 +0300

    feat(scrollToTop): добавить компонент scrollToTop и обновить layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { TabBar } from "./tabBar";
+import { ScrollToTop } from "./scrollToTop";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <ScrollToTop/>
       <Header />
       <main>{children}</main>
       <TabBar/>
