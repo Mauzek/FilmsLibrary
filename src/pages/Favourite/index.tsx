4a497dc2bbd7210c119e6e0549d5f9bbf345cdc0
Mauzek <artemilliy@gmail.com>
2025-07-14 14:49:06 +0300

    feat(favouritePage): добавить страницу favouritePage

--- src/pages/Favourite/index.tsx
+++ src/pages/Favourite/index.tsx
@@ -1,9 +1,28 @@
-import styles from './favourite.module.scss';
+import { useEffect } from "react";
+import { EmptyState, MoviesGrid, Section } from "@/components";
+import { Icon28LikeOutline } from "@vkontakte/icons";
+import { useFavouriteStore } from "@/store";
 
 export const FavouritePage = () => {
+  const { favourites } = useFavouriteStore();
+
+  useEffect(() => {
+    document.title = "Избранное на VK FilmsLib";
+  }, []);
+
   return (
-    <section className={styles.page}>
-        favouritePage
-    </section>
-  )
-}
+    <main>
+      <Section title="Избранное" icon={<Icon28LikeOutline />}>
+        {favourites.length > 0 ? (
+          <MoviesGrid movies={favourites} />
+        ) : (
+          <EmptyState
+            title="Список избранного пуст"
+            description="Вы можете добавить фильмы в избранное, чтобы они отображались здесь"
+            icon={<Icon28LikeOutline />}
+          />
+        )}
+      </Section>
+    </main>
+  );
+};
