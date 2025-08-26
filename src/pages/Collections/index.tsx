d0df53dfaf073c72d0debd77d5c7ccc2c70c270d
Artyom Illiy <artemilliy@gmail.com>
2025-08-26 23:30:42 +0300

    feat(collectionsPage): добавить функционал на странице collectionsPage

--- src/pages/Collections/index.tsx
+++ src/pages/Collections/index.tsx
@@ -1,7 +1,47 @@
+import { useCallback, useMemo, useState } from "react";
+import {
+  CollectionCard,
+  Grid,
+  Section,
+  CollectionsNavigation,
+} from "@/components";
+import listsData from "@data/lists.json";
+import { Icon28PlayRectangleStackOutline } from "@vkontakte/icons";
 
+export const CollectionsPage = () => {
+  const { collections, categories } = listsData;
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
 
-export const Collections = () => {
+  const filteredCollections = useMemo(
+    () =>
+      activeCategory
+        ? collections.filter((c) => c.category === activeCategory)
+        : collections,
+    [activeCategory, collections]
+  );
+
+  const handleSelectCategory = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      const category = e.currentTarget.dataset.value || null;
+      setActiveCategory((prev) => (prev !== category ? category : null));
+    },
+    []
+  );
+
   return (
-    <div>Collections</div>
-  )
-}
+    <main>
+      <Section title="Коллекции" icon={<Icon28PlayRectangleStackOutline />}>
+        <CollectionsNavigation
+          activeCategory={activeCategory}
+          categories={categories}
+          onSelect={handleSelectCategory}
+        />
+        <Grid columns={5}>
+          {filteredCollections.map((collection) => (
+            <CollectionCard key={collection.slug} {...collection} />
+          ))}
+        </Grid>
+      </Section>
+    </main>
+  );
+};
