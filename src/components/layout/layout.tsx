63229b34c282949c596974299cfb711154c0dbd9
Artyom Illiy <artemilliy@gmail.com>
2025-08-28 15:54:37 +0300

    feat(analytics): добавлена аналитика от vercel

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,10 +3,12 @@ import { Header } from "./header";
 import { Footer } from "./footer";
 import { TabBar } from "./tabBar";
 import { ScrollToTop } from "./scrollToTop";
+import { Analytics } from '@vercel/analytics/react';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Analytics/>
       <ScrollToTop/>
       <Header />
       {children}
