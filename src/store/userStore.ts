24ad0402597dcfe70c5f775c114597d7352de6c0
Artyom Illiy <artemilliy@gmail.com>
2025-09-12 16:06:05 +0300

    refactor(userStore): удалить mocks

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,26 +1,6 @@
 import { makeAutoObservable } from "mobx";
 import type { User } from "@/types";
 
-const mockGoogleProvider = {
-  providerId: "google.com",
-  uid: "1234567890",
-  displayName: "Алексей Петров",
-  email: "alexey.petrov@gmail.com",
-  phoneNumber: null,
-  photoURL: "https://lh3.googleusercontent.com/.../photo.jpg",
-};
-
-export const userMock = {
-  uid: "abc123xyz456",
-  email: "john.doe@example.com",
-  displayName: "John Doe",
-  photoURL: "https://lh3.googleusercontent.com/ogw/AF2bZyixcamI8keFR7ndMQvozMvQ95yloCYu_ay5ibrJGe7EgZs=s32-c-mo",
-  emailVerified: true,
-  isAnonymous: false,
-  providerData: [mockGoogleProvider],
-  createdAt: "2025-04-01T10:00:00.000Z",
-};
-
 export class UserStore  {
   user: User | null = null;
   isAuthenticated = false;
