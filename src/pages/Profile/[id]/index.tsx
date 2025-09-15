5e2ca7533d111a8776d111b8cdc5e5a1ef6d7aaa
Artyom Illiy <artemilliy@gmail.com>
2025-09-15 20:11:29 +0300

    feat(profilePage): добавить получение данных пользователя, обработку ошибок и отображение

--- src/pages/Profile/[id]/index.tsx
+++ src/pages/Profile/[id]/index.tsx
@@ -1,8 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useUserProfile } from "@/hooks/useUserProfile";
+import { ErrorState, UserDetails } from "@/components";
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
-}
+  const { id } = useParams<{ id: string }>();
+  const { user, lists, loadingProfile, loadingLists, error } =
+    useUserProfile(id);
 
-export default ProfilePage
+  if (error || (!user && !loadingProfile))
+    return (
+      <ErrorState
+        error="400"
+        title="Пользователь не найден"
+        description="Похоже, пользователь которого вы ищите, не существует"
+      />
+    );
+
+  return (
+    <main>
+      <UserDetails
+        data={user!}
+        lists={lists}
+        loadingLists={loadingLists}
+        loadingProfile={loadingProfile}
+        userId={id!}
+      />
+    </main>
+  );
+};
+
+export default ProfilePage;
