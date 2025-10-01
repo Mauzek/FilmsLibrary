ebc11c5da13932477beb5b18c75085350c0c72b4
Artyom Illiy <artemilliy@gmail.com>
2025-10-01 16:34:10 +0300

    feat(user types): обновить и добавить новые типы для стейта пользователя

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,5 @@
 import type { UserInfo } from "firebase/auth";
+import type { Movie } from "./movie";
 
 export interface User {
   uid: string;
@@ -9,10 +10,25 @@ export interface User {
   isAnonymous: boolean;
   providerData: UserInfo[];
   createdAt: string;
+  profileColor: string | null;
 }
 
 export interface UserState {
   user: User | null;
   isAuthenticated: boolean;
   loading: boolean;
+  lists: Record<string, Movie[]>;
+}
+
+interface Statistic {
+  count: number;
+  percent: number;
+}
+
+export interface UserListsStatistics {
+  favorites: Statistic;
+  planned: Statistic;
+  watched: Statistic;
+  dropped: Statistic;
+  all: number;
 }
