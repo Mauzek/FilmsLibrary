5aa5ea96fbf3d17e62910b36db92763b8fb0be76
Artyom Illiy <artemilliy@gmail.com>
2025-10-01 16:33:14 +0300

    feat(userStore): добавить хранение списков пользователей в глобальный стейт

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,10 +1,18 @@
 import { makeAutoObservable } from "mobx";
-import type { User } from "@/types";
+import type { Movie, User } from "@/types";
 
-export class UserStore  {
+type ListName = "favorites" | "planned" | "dropped" | "watched";
+
+export class UserStore {
   user: User | null = null;
   isAuthenticated = false;
-  loading = true; 
+  lists: Record<ListName, Movie[]> = {
+    favorites: [],
+    planned: [],
+    dropped: [],
+    watched: [],
+  };
+  loading = true;
 
   constructor() {
     makeAutoObservable(this);
@@ -16,6 +24,41 @@ export class UserStore  {
     this.loading = false;
   };
 
+  setLists = (lists: Partial<Record<ListName, Movie[]>>) => {
+    this.lists = {
+      favorites: lists.favorites ?? this.lists.favorites,
+      planned: lists.planned ?? this.lists.planned,
+      dropped: lists.dropped ?? this.lists.dropped,
+      watched: lists.watched ?? this.lists.watched,
+    };
+  };
+
+  addMovieToList = (listName: ListName, movie: Movie) => {
+    const { id } = movie;
+    let foundIn: ListName | null = null;
+
+    for (const key in this.lists) {
+      const exists = this.lists[key as ListName].some((m) => m.id === id);
+      if (exists) {
+        foundIn = key as ListName;
+        break;
+      }
+    }
+
+    if (foundIn) {
+      this.lists[foundIn] = this.lists[foundIn].filter((m) => m.id !== id);
+    }
+
+    const alreadyInTarget = this.lists[listName].some((m) => m.id === id);
+    if (!alreadyInTarget) {
+      this.lists[listName].push(movie);
+    }
+  };
+
+  removeMovieFromList = (listName: ListName, movieId: number) => {
+    this.lists[listName] = this.lists[listName].filter((m) => m.id !== movieId);
+  };
+
   setLoading = (loading: boolean) => {
     this.loading = loading;
   };
@@ -24,5 +67,11 @@ export class UserStore  {
     this.user = null;
     this.isAuthenticated = false;
     this.loading = false;
+    this.lists = {
+      favorites: [],
+      planned: [],
+      dropped: [],
+      watched: [],
+    };
   };
 }
